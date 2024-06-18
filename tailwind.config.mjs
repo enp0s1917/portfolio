56fab0e6e782f2f37b825feffbf41b77299883c5
enp0s <pnipe97@gmail.com>
2024-06-18 14:29:04 +0200

    last fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'colorFondo': '#09304a',
+			'pinky': '#E6B4CA',
+			'colorLetra': '#BDBDBD',
+			'amarillo': '#A3DDB9',
+			'fuerte': '#333333',
+		},
 		extend: {},
 	},
+	
 	plugins: [],
 }
